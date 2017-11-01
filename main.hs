fd43cdf27d6d3454a2ff4e3f2b94eaf62f52d4a6
John Doe <john@doe.org>
2017-11-01 11:05:59 +0100

    code cleanup

--- main.hs
+++ main.hs
@@ -6,43 +6,30 @@
 {-# LANGUAGE MultiParamTypeClasses #-} -- RawHtml
 
 module Main where
---import NLP.Hext.NaiveBayes
+
 import Data.Char
 import Data.List
 import Data.Function (on)
-import GHC.Generics (Generic)
 import Data.Text (Text)
+import Data.Proxy (Proxy(..))
+import Data.Aeson (ToJSON, toJSON, object, (.=), FromJSON, parseJSON, (.:), withObject, Object)
+import GHC.Generics (Generic)
 import qualified Data.Text.Lazy as T
-import qualified Data.ByteString.Lazy as U
-import qualified Data.ByteString.Char8 as BS
-import Codec.CBOR as CBOR
-import Codec.Serialise
-import Data.Random.Extras
-import System.Random
+
 import System.IO
 import Numeric
-import Data.Aeson (ToJSON, toJSON, object, (.=), FromJSON, parseJSON, (.:), withObject, Object)
-import Network.Wai.Handler.Warp (run)
-import Data.Proxy (Proxy(..))
-import           Servant.API
-import           Servant.Client
+
+import Servant.API
 import Servant.Server
 import Servant.HTML.Lucid
 import Lucid
-import Lucid.Base
-
-import Servant.Utils.StaticFiles
+import Network.HTTP.Media  ((//), (/:))
+import Network.Wai.Handler.Warp (run)
 
 -- Hext
+import NLP.Hext.NaiveBayes
 import qualified Data.HashMap.Lazy as H
 import qualified Data.Set as S
-import Data.Maybe
-import Data.Char
-import Data.Function
-import Data.List
-import qualified Data.Text.Lazy as T
-import Data.Monoid
-import Network.HTTP.Media  ((//), (/:))
 
 
 autoFill [] foo = []
@@ -153,60 +140,11 @@ type LabelsAPI =
   "labels" :> ReqBody '[JSON] Ad :> Post '[JSON] Label :<|>
   "index" :> Get '[HTML] Page
 
--- | A hash representing frequency list of words
-type FrequencyList = H.HashMap T.Text Int
-
--- | A frequency list of words that has been assigned a class
-data Labeled a = Labeled { hash :: FrequencyList -- ^ a frequency list
-                         , label :: a -- ^ the class label for a piece of text
-                         } 
-
--- | A class which has a specific probability of occurring
-data Classified a = Classified { _class :: a
-                               , probability :: Double } deriving (Eq)
-
--- | A model representing the knowledge that has been given
-data BayesModel a = BayesModel { classes :: S.Set a -- ^ a set of user-defined classes
-                               , vocab :: FrequencyList -- ^ the frequency list of all vocabulary
-                               , material :: [Labeled a] -- ^ a list of all of the classified text
-                                     }
-
-instance (Show a) => Show (BayesModel a) where
-  show model = show (classes model) ++
-        " " ++ show (vocab model)
-
-instance (Eq a) => Ord (Classified a) where compare = compare `on` probability
-
-instance (Show a) => Show (Classified a) where
-  show c = show (_class c, probability c)
-
-instance (Ord a) => Monoid (BayesModel a) where
-  mempty = emptyModel
-  a `mappend` b =
-   BayesModel (S.union (classes a) (classes b)) (H.union (vocab a) (vocab b)) ((material a) ++ (material b))
 
 
--- | an empty model to begin teaching
-emptyModel :: BayesModel a
-emptyModel = BayesModel S.empty H.empty []
-
--- | teaches the model
-teach :: (Ord a) => T.Text -- ^ the sample
-                 -> a -- ^ sample's class
-                  -> BayesModel a -- ^ the current model
-                  -> BayesModel a -- ^ the new model
-teach sample c model = 
-  let fl = vectorize sample
-      lb = [Labeled fl c]
-      cl = S.singleton c
-  in (BayesModel cl fl lb) <> model
-
--- | Runs a sample string through the Naive Bayes algorithm using
--- a model containing all knowledge from previous learning
-runBayes :: (Ord a, Eq a) => BayesModel a  -- ^ a model that has been taught using 'learn'
-            -> String -- ^ the sample string to be classified
-            -> a -- ^ a datatype representing a class to classify text
-runBayes model sample = argmax $ classify model (T.words $ T.pack sample)
+--
+-- try compute score using prob field from NLP.Hext.NaiveBayes
+--
 
 runBayesWithScore model sample = r where
   max = S.findMax $ classify model (T.words $ T.pack sample)
@@ -214,7 +152,6 @@ runBayesWithScore model sample = r where
 
 runBayesWithAllScore model sample = S.elems (classify model (T.words $ T.pack sample))
 
-classify :: (Ord a, Eq a) => BayesModel a -> [T.Text] -> S.Set (Classified a)
 classify model = f where
     cs = classes model
     lengthVocab = H.size $ vocab model
@@ -240,17 +177,8 @@ pWordGivenClass w denom currentCase =
     (fromIntegral (nk + 1)) / (fromIntegral denom) where
         nk = totalOfWord w currentCase    
 
--- returns the class that which has the highest probability associated with it
-argmax :: (Eq a) => S.Set (Classified a) -> a
-argmax = _class . S.findMax
-
 removePunctuation :: T.Text -> T.Text
 removePunctuation = T.filter (not . isPunctuation)
-
--- takes a list of words and makes a frequency list
-vectorize :: T.Text -> FrequencyList
-vectorize = 
-    H.fromListWith (+) . flip zip (repeat 1) . T.words . removePunctuation
 
 -- a list of frequency lists, derived from a set of material
 vecs :: [Labeled a] -> [FrequencyList]
